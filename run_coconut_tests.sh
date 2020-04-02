5ed76de2d0b8daa2e00fd491bb65e562fb220827
Toon Demeester <toon.demeester@ugent.be>
2020-04-02 18:49:43 +0200

    Add more print-outs in run_coconut_tests.sh

--- run_coconut_tests.sh
+++ run_coconut_tests.sh
@@ -3,11 +3,14 @@
 # add coconut installation path to parent directory of coconut
 COCONUT_INSTALLATION_PATH=$(dirname $(pwd))
 export PYTHONPATH=$COCONUT_INSTALLATION_PATH:$PYTHONPATH
+
 #run tests
+echo "Running tests..."
 cd $COCONUT_INSTALLATION_PATH/coconut/tests
 python3 test_coconut.py
 
 # run an FSI test example
+echo "Running test example..."
 cd $COCONUT_INSTALLATION_PATH/coconut/test_examples/tube_tube_flow_tube_structure
 chmod 700 setup_tube_flow.sh setup_tube_structure.sh
 ./setup_tube_flow.sh
