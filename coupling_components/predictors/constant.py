9ca903f1e72096c9999eccc8d601f8d11ec48519
Nicolas Delaissé <56599042+nicolasdlss@users.noreply.github.com>
2020-12-09 17:16:52 +0100

    Add constant predictor

--- coupling_components/predictors/constant.py
+++ coupling_components/predictors/constant.py
@@ -2,18 +2,15 @@ from coconut.coupling_components.predictors.predictor import Predictor
 
 
 def Create(parameters):
-    return PredictorLinear(parameters)
+    return PredictorConstant(parameters)
 
 
 # Class PredictorLinear: Linear extrapolation based on the last two time steps, assuming constant time step size.
-class PredictorLinear(Predictor):
+class PredictorConstant(Predictor):
     def __init__(self, _unused):
         super().__init__(_unused)
 
-        self.order = 1
+        self.order = 0
 
     def Predict(self, x):
-        if len(self.dataprev) == 1:
-            return self.Constant(x)
-        else:
-            return self.Linear(x)
+        return self.Constant(x)
