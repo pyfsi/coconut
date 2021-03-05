23897d7dfcedb4ff04e523366d7199c4741819da
Nicolas Delaissé <56599042+nicolasdlss@users.noreply.github.com>
2021-03-05 17:52:38 +0100

    Add division by fluid density

--- coupling_components/solver_wrappers/python/tube_ringmodel_solver.py
+++ coupling_components/solver_wrappers/python/tube_ringmodel_solver.py
@@ -46,8 +46,8 @@ class SolverWrapperTubeRingmodel(Component):
 
         # initialization
         self.areference = np.pi * d ** 2 / 4  # reference area of cross section
-        self.p = np.ones(self.m) * self.preference  # kinematic pressure
-        self.a = np.ones(self.m) * self.areference  # area of cross section
+        self.p = np.zeros(self.m) * self.preference  # kinematic pressure
+        self.a = np.zeros(self.m) * self.areference  # area of cross section
         self.c02 = self.cmk2 - self.preference / 2  # wave speed squared with reference pressure
 
         self.disp = np.zeros((self.m, 3))  # displacement
@@ -85,7 +85,7 @@ class SolverWrapperTubeRingmodel(Component):
     def solve_solution_step(self, interface_input):
         # input
         self.interface_input = interface_input.copy()
-        self.p = interface_input.get_variable_data("wall", "pressure").flatten()
+        self.p = interface_input.get_variable_data("wall", "pressure").flatten() / self.rhof
 
         # independent rings model
         for i in range(len(self.p)):
