600f9758b0c9c1102ba99096b7122e122a2ad7ce
Nicolas Delaissé <56599042+nicolasdlss@users.noreply.github.com>
2021-03-29 18:02:16 +0200

    Add equal test to Interface

--- tests/data_structure/test_interface.py
+++ tests/data_structure/test_interface.py
@@ -9,18 +9,31 @@ class TestInterface(unittest.TestCase):
 
     def setUp(self):
         self.model_part_size = model_part_size = 3
-        self.parameters = {'interface_a':
-                           [
-                            {'model_part': 'mp1', 'variables': ['pressure', 'traction']},
-                            {'model_part': 'mp2', 'variables': ['density']}
-                            ]
-                           }
+        self.parameters = {
+            'interface_a':
+                [
+                    {'model_part': 'mp1', 'variables': ['pressure', 'traction']},
+                    {'model_part': 'mp2', 'variables': ['density']}
+                ],
+            'interface_b':
+                [
+                    {'model_part': 'mp1', 'variables': ['pressure', 'displacement']},
+                    {'model_part': 'mp2', 'variables': ['density']}
+                ],
+
+            'interface_c':
+                [
+                    {'model_part': 'mp1', 'variables': ['pressure', 'traction']},
+                    {'model_part': 'mp3', 'variables': ['density']}
+                ]
+        }
 
         self.model = Model()
-        x0 = np.random.rand(3 * model_part_size)
-        y0 = np.random.rand(3 * model_part_size)
-        z0 = np.random.rand(3 * model_part_size)
-        ids = np.arange(0, model_part_size)
+        self.x0 = x0 = np.random.rand(3 * model_part_size)
+        self.y0 = y0 = np.random.rand(3 * model_part_size)
+        self.z0 = z0 = np.random.rand(3 * model_part_size)
+        self.ids = ids = np.arange(0, model_part_size)
+        np.random.shuffle(ids)
         self.model.create_model_part('mp1', x0[0:model_part_size], y0[0:model_part_size], z0[0:model_part_size], ids)
         self.model.create_model_part('mp2', x0[model_part_size:2 * model_part_size],
                                      y0[model_part_size:2 * model_part_size],
@@ -171,6 +184,34 @@ class TestInterface(unittest.TestCase):
         interface2 = self.interface.copy()
         interface2.set_interface_data(interface_data2)
         return interface_data1, interface_data2, interface1, interface2
+
+    def test_eq(self):
+        self.interface.set_interface_data(self.interface_data)
+
+        model_part_size = self.model_part_size
+        x0 = self.x0
+        y0 = self.y0
+        z0 = self.z0
+        ids = self.ids
+        model2 = Model()
+        model2.create_model_part('mp1', x0[0:model_part_size], y0[0:model_part_size], z0[0:model_part_size], ids)
+        model2.create_model_part('mp2', x0[model_part_size:2 * model_part_size],
+                                 y0[model_part_size:2 * model_part_size],
+                                 z0[model_part_size:2 * model_part_size], ids)
+        interface2 = Interface(self.parameters['interface_a'], model2)
+        interface2.set_interface_data(self.interface_data)
+        self.assertIsNot(self.interface, interface2)
+        self.assertEqual(self.interface, interface2)
+
+        interface3 = Interface(self.parameters['interface_b'], model2)
+        interface3.set_interface_data(self.interface_data)
+        self.assertNotEqual(self.interface, interface3)
+
+        interface4 = interface2.copy()
+        interface_data4 = self.interface_data.copy()
+        interface_data4[np.random.randint(interface4.size)] = np.random.rand()
+        interface4.set_interface_data(interface_data4)
+        self.assertNotEqual(self.interface, interface4)
 
     def test_add(self):
         interface_data1, interface_data2, interface1, interface2 = self.create_test_interfaces()
