ba4fa0ba5ff42a41102f6bc6a044d25e22eac8c1
Nicolas Delaissé <56599042+nicolasdlss@users.noreply.github.com>
2021-04-13 10:02:47 +0200

    Add Todos

--- examples/setup_files/fluent2d/setup_fluent2d.sh
+++ examples/setup_files/fluent2d/setup_fluent2d.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # make fluent case
+#TODO remove module load command
 ml ANSYS_CFD/2019R1
 fluent 2ddp -g -i case.jou > setup_fluent.log 2>&1
 ml -ANSYS_CFD
--- examples/setup_files/openfoam3d/setup_openfoam3d.sh
+++ examples/setup_files/openfoam3d/setup_openfoam3d.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+#TODO remove module load command
 ml OpenFOAM/4.1
 source $FOAM_BASH
 wmake ../../../coupling_components/solver_wrappers/openfoam/CoCoNuT_pimpleFoam/ > wmake_log
