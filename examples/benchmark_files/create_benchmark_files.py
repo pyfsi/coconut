9b86365c115f45db546b9bfe659f8b63f87ad4fd
Nicolas Delaissé <56599042+nicolasdlss@users.noreply.github.com>
2021-04-17 21:28:10 +0200

    Add documentation and small updates

--- examples/benchmark_files/create_benchmark_files.py
+++ examples/benchmark_files/create_benchmark_files.py
@@ -8,14 +8,45 @@ import subprocess
 from shutil import copy2
 from os.path import join
 
+
+"""
+This file creates benchmark pickle files used by the script 'evaluate_examples.py'.
+
+Running this file creates pickle files for the examples in the dictionary 'examples'.
+The key is the folder name of the example and the corresponding value is an int indicating the number of 
+time steps the example should be run.
+In order to exclude some examples, comment out the corresponding key-value pair in the dictionary 'examples'.
+
+To include another example, simply add a new key-value pair to the dictionary 'examples'.
+"""
+
+
 # set paths
 execute_path = os.path.realpath(os.path.dirname(__file__))
 tmp_path = join(execute_path, 'tmp')
-examples_path = os.path.realpath(join('..', os.path.dirname(__file__)))
+examples_path = os.path.realpath(join(os.path.dirname(__file__), '..'))
 bench_mark = join(examples_path, 'benchmark_files')
+
+# example and tuple with number of time steps and optional additional files
+# comment out examples that you do not want to make benchmark files for
+examples = {
+    'tube_fluent2d_abaqus2d': 2,
+    'tube_fluent2d_abaqus2d_steady': 1,
+    'tube_fluent2d_tube_structure': 2,
+    'tube_fluent3d_abaqus2d': 2,
+    'tube_fluent3d_abaqus3d': 2,
+    'tube_fluent3d_kratos_structure3d': 2,
+    'tube_openfoam3d_abaqus3d': 2,
+    'tube_openfoam3d_kratos_structure3d': 2,
+    'tube_tube_flow_abaqus2d': 2,
+    'tube_tube_flow_tube_ringmodel': 100,
+    'tube_tube_flow_tube_structure': 100
+    }
 
 
 def set_up():
+    # if os.path.exists(tmp_path):
+    #     shutil.rmtree(tmp_path)
     os.mkdir(tmp_path)
     cmd = f'cp -r {join(examples_path, "setup_files")} {join(tmp_path, "setup_files")}'
     p = subprocess.Popen(cmd, executable='/bin/bash', cwd=tmp_path, shell=True)
@@ -44,6 +75,8 @@ def create_benchmark(example, number_of_timesteps):
     with open(join(tmp_example_path, parameter_file_name), 'r') as parameter_file:
         parameters = json.load(parameter_file)
     parameters['settings']['number_of_timesteps'] = number_of_timesteps
+    parameters['coupled_solver']['settings']['save_results'] = True
+    parameters['coupled_solver']['settings']['name'] = 'case_results'  # TODO: modify to 'case' if restart is merged
 
     # perform simulation
     os.chdir(tmp_example_path)
@@ -52,25 +85,8 @@ def create_benchmark(example, number_of_timesteps):
     os.chdir(execute_path)
 
     # move and rename results data
-    shutil.move(join(tmp_example_path, 'results.pickle'), join(bench_mark, f'{example}.pickle'))
+    shutil.move(join(tmp_example_path, 'case_results.pickle'), join(bench_mark, f'{example}.pickle'))
 
-
-# example and number of time steps
-# comment out examples that you do not want to make benchmark files for
-examples = {
-    'test_single_solver': 2,
-    'tube_fluent2d_abaqus2d': 2,
-    'tube_fluent2d_abaqus2d_steady': 1,
-    'tube_fluent2d_tube_structure': 2,
-    'tube_fluent3d_abaqus2d': 2,
-    'tube_fluent3d_abaqus3d': 2,
-    'tube_fluent3d_kratos_structure3d': 2,
-    'tube_openfoam3d_abaqus3d': 2,
-    'tube_openfoam3d_kratos_structure3d': 2,
-    'tube_tube_flow_abaqus2d': 2,
-    'tube_tube_flow_tube_ringmodel': 100,
-    'tube_tube_flow_tube_structure': 100
-    }
 
 set_up()
 for example, number_of_timesteps in examples.items():
--- examples/evaluate_examples.py
+++ examples/evaluate_examples.py
@@ -11,6 +11,25 @@ from unittest import TestSuite
 from os.path import join
 
 
+"""
+This file runs the example cases and compares the results and convergence history, with previously save benchmark
+pickle files.
+
+Run this file with the unittest framework (see unittest documentation for command-line flags):
+    python -m unittest evaluate_examples.py -bv
+In order to exclude some examples from running, comment out the corresponding class in the tuple 'test_cases'.
+
+To include another example, create a new class analogous to the existing classes and add the class name to the tuple
+'test_cases'. The class variable 'example' is the folder name and the class variable 'number_of_timesteps' refers to the 
+number of time steps that the example should be run (the benchmark file should have run for at least this number of 
+time steps).
+There are two other class variables; 'additional_files' is a list of additional files in the example required for the 
+simulation besides 'setup.sh' and 'parameters.json', and 'compare_data' is a boolean which determines whether the
+comparison with a benchmark pickle file should be made.
+Once this class is made, you may need to create a benchmark file for this new example using the corresponding script.
+"""
+
+
 # set paths
 execute_path = os.path.realpath(os.path.dirname(__file__))
 tmp_path = join(execute_path, 'tmp')
@@ -19,6 +38,8 @@ bench_mark = join(examples_path, 'benchmark_files')
 
 
 def setUpModule():
+    # if os.path.exists(tmp_path):
+    #     shutil.rmtree(tmp_path)
     os.mkdir(tmp_path)
     cmd = f'cp -r {join(examples_path, "setup_files")} {join(tmp_path, "setup_files")}'
     p = subprocess.Popen(cmd, executable='/bin/bash', cwd=tmp_path, shell=True)
@@ -32,6 +53,8 @@ def tearDownModule():
 class EvaluateExamples(unittest.TestCase):
     example = ''
     number_of_timesteps = 5
+    additional_files = []
+    compare_data = True
 
     @classmethod
     def setUpClass(cls):
@@ -40,7 +63,7 @@ class EvaluateExamples(unittest.TestCase):
 
         # copy example folder to tmp
         os.mkdir(tmp_example_path)
-        for file in ('parameters.json', 'setup.sh'):
+        for file in ['parameters.json', 'setup.sh'] + cls.additional_files:
             shutil.copy2(join(examples_path, cls.example, file), tmp_example_path)
 
         # perform set up
@@ -52,6 +75,8 @@ class EvaluateExamples(unittest.TestCase):
         with open(join(tmp_example_path, parameter_file_name), 'r') as parameter_file:
             parameters = json.load(parameter_file)
         parameters['settings']['number_of_timesteps'] = cls.number_of_timesteps
+        parameters['coupled_solver']['settings']['save_results'] = True
+        parameters['coupled_solver']['settings']['name'] = 'case_results'  # TODO: modify to 'case' if restart is merged
 
         # perform simulation
         os.chdir(tmp_example_path)
@@ -59,28 +84,37 @@ class EvaluateExamples(unittest.TestCase):
         simulation.run()
         os.chdir(execute_path)
 
-        # read results data
-        with open(join(tmp_example_path, 'results.pickle'), 'rb') as f:
-            cls.results = pickle.load(f)
+        if cls.compare_data:
+            # read results data
+            with open(join(tmp_example_path, 'case_results.pickle'), 'rb') as f:
+                cls.results = pickle.load(f)
 
-        # read benchmark data
-        with open(join(bench_mark, f'{cls.example}.pickle'), 'rb') as f:
-            cls.benchmark = pickle.load(f)
+            # read benchmark data
+            with open(join(bench_mark, f'{cls.example}.pickle'), 'rb') as f:
+                cls.benchmark = pickle.load(f)
 
     def test_solution(self):
-        for interface in ('solution_x', 'solution_y'):
-            np.testing.assert_almost_equal(self.benchmark[interface][:, :self.number_of_timesteps],
-                                           self.results[interface][:, :self.number_of_timesteps])
+        if self.compare_data:
+            for interface in ('solution_x', 'solution_y'):
+                np.testing.assert_almost_equal(self.benchmark[interface][:, :self.number_of_timesteps],
+                                               self.results[interface][:, :self.number_of_timesteps])
+        else:
+            self.skipTest(f'Solution not compared for {self.__class__.__name__}')
 
     def test_convergence_history(self):
-        for time_step in range(self.number_of_timesteps):
-            np.testing.assert_allclose(np.array(self.benchmark['residual'][time_step]),
-                                       np.array(self.results['residual'][time_step]))
+        if self.compare_data:
+            for time_step in range(self.number_of_timesteps):
+                np.testing.assert_allclose(np.array(self.benchmark['residual'][time_step]),
+                                           np.array(self.results['residual'][time_step]))
+        else:
+            self.skipTest(f'Convergence history not compared for {self.__class__.__name__}')
 
 
 class TestTestSingleSolver(EvaluateExamples):
     example = 'test_single_solver'
     number_of_timesteps = 2
+    additional_files = ['dummy_solver.py']
+    compare_data = False
 
 
 class TestTubeFluent2DAbaqus2D(EvaluateExamples):
@@ -151,7 +185,7 @@ test_cases = (
     TestTubeOpenFOAM3DKratosStructure3D,
     TestTubeTubeFlowAbaqus2D,
     TestTubeTubeFlowTubeRingmodel,
-    TestTubeTubeFlowTubeStructure
+    TestTubeTubeFlowTubeStructure,
 )
 
 
