1c4ade184a344f4557575afbccb44aa49c944755
hdolfen <52496779+hdolfen@users.noreply.github.com>
2021-05-04 22:21:29 +0200

    Delete superclass to avoid the tests to be executed

--- tests/solver_wrappers/fluent/test_v2020R1.py
+++ tests/solver_wrappers/fluent/test_v2020R1.py
@@ -14,5 +14,7 @@ class TestSolverWrapperFluent2020R1Tube3D(TestSolverWrapperFluent2019R1Tube3D):
     setup_case = True
 
 
+del TestSolverWrapperFluent2019R1Tube2D, TestSolverWrapperFluent2019R1Tube3D  # to avoid executing 2019 test too
+
 if __name__ == '__main__':
     unittest.main()
