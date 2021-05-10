8852ac4956b4f59446aef26ea7fe9c2e0772e4a6
hdolfen <52496779+hdolfen@users.noreply.github.com>
2021-05-10 14:05:33 +0200

    Change import to avoid discovery of superclass test

--- tests/solver_wrappers/fluent/test_v2020R1.py
+++ tests/solver_wrappers/fluent/test_v2020R1.py
@@ -1,20 +1,17 @@
-from coconut.tests.solver_wrappers.fluent.test_v2019R1 \
-    import TestSolverWrapperFluent2019R1Tube2D, TestSolverWrapperFluent2019R1Tube3D
+import coconut.tests.solver_wrappers.fluent.test_v2019R1 as TubeTest
 
 import unittest
 
 
-class TestSolverWrapperFluent2020R1Tube2D(TestSolverWrapperFluent2019R1Tube2D):
+class TestSolverWrapperFluent2020R1Tube2D(TubeTest.TestSolverWrapperFluent2019R1Tube2D):
     version = '2020R1'
     setup_case = True
 
 
-class TestSolverWrapperFluent2020R1Tube3D(TestSolverWrapperFluent2019R1Tube3D):
+class TestSolverWrapperFluent2020R1Tube3D(TubeTest.TestSolverWrapperFluent2019R1Tube3D):
     version = '2020R1'
     setup_case = True
 
-
-del TestSolverWrapperFluent2019R1Tube2D, TestSolverWrapperFluent2019R1Tube3D  # to avoid executing 2019R1 tests too
 
 if __name__ == '__main__':
     unittest.main()
