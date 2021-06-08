ed8148f7e76745130ae019319cb865fa434677b2
hdolfen <52496779+hdolfen@users.noreply.github.com>
2021-06-08 10:22:46 +0200

    Change to redundant licensing upon loading module

--- solver_modules.py
+++ solver_modules.py
@@ -14,13 +14,14 @@ solver_load_cmd_dict = {
         'fluent.v2019R3': 'ml ANSYS_CFD/2019R3',
         'fluent.v2020R1': 'ml ANSYS_CFD/2020R1',
         'abaqus.v614': 'ml intel/2018a && ml ABAQUS/6.14',
+        'abaqus.v614_branch': 'ml intel/2018a && ml ABAQUS/6.14',
         'kratos.structural_mechanics_application.v60': 'ml Kratos/6.0-foss-2018a-Python-3.6.4',
         'openfoam.v41': 'ml OpenFOAM/4.1 && source $FOAM_BASH'
     },
     'ugent_cluster_CO7': {
         'fluent.v2019R3': 'ml ANSYS_CFD/2019R3',
         'fluent.v2020R1': 'ml ANSYS_CFD/2020R1',
-        'abaqus.v614': 'ml intel/2018a && ml ABAQUS/6.14',
+        'abaqus.v614': 'ml intel/2018a && ml ABAQUS/6.14 && export LM_LICENSE_FILE=@ir03lic1.ugent.be:@bump.ugent.be',
         'kratos.structural_mechanics_application.v60': 'ml Kratos/6.0-foss-2018a-Python-3.6.4',
         'openfoam.v41': 'ml OpenFOAM/4.1-foss-2016b && source $FOAM_BASH'
     },
@@ -28,7 +29,8 @@ solver_load_cmd_dict = {
         'fluent.v2019R1': 'ml ANSYS_CFD/2019R1',
         'fluent.v2019R2': 'ml ANSYS_CFD/2019R2',
         'fluent.v2019R3': 'ml FLUENT/2019R3',
-        'abaqus.v614': 'ml intel/2018a && ml ABAQUS/6.14.1-linux-x86_64 && unset SLURM_GTIDS',
+        'abaqus.v614': 'ml intel/2018a && ml ABAQUS/6.14.1-linux-x86_64 && unset SLURM_GTIDS && export LM_LICENSE_FILE='
+                       '@ir03lic1.ugent.be:@bump.ugent.be',
         'kratos.structural_mechanics_application.v60': 'ml Kratos/6.0-foss-2018a-Python-3.6.4',
     }
 }
