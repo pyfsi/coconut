3c69471b9c35d0fabcc412d8385a7932abcc6e15
Nicolas Delaissé <56599042+nicolasdlss@users.noreply.github.com>
2021-06-23 11:28:24 +0200

    Fix step variable

--- examples/setup_files/turek/fluent2d/create_movie/images.py
+++ examples/setup_files/turek/fluent2d/create_movie/images.py
@@ -4,7 +4,7 @@ import imageio
 step = 50
 number_of_frames = 350
 for variable in ('pressure', 'velocity'):
-    file_names = [f'../case_timestep{i * 50}-{variable}.jpg' for i in range(1, number_of_frames + 1)]
+    file_names = [f'../case_timestep{i * step}-{variable}.jpg' for i in range(1, number_of_frames + 1)]
     with imageio.get_writer(f'movie-{variable}.gif', mode='I') as writer:
         for file_name in file_names:
             image = imageio.imread(file_name)
