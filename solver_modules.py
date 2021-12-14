514e5c171498f6f5447f80dbafeefa7bd75c7994
hdolfen <52496779+hdolfen@users.noreply.github.com>
2021-12-14 12:01:53 +0100

    Fix typo and simplify

--- solver_modules.py
+++ solver_modules.py
@@ -41,9 +41,8 @@ solver_load_cmd_dict = {
     },
     'breniac': {
         'fluent.v2021R1': 'module load FLUENT/2021R1 && export ANSYSLI_SERVERS=2325@ir03lic1.ugent.be '
-                          '&& export ANSYSLMD_LICENSE_FILE=1055@ir03lic1.ugent.be '
-                          '&& cat $PBS_NODEFILE | sed -e "s/\\.$CLUSTER_NAME.*$//" > fluent.hosts',
-        'abaqus.v614': 'module load intel/2018a && module load ABAQUS/6.14.1-linux-x86_64 && unset SLURM_GITDS '
+                          '&& export ANSYSLMD_LICENSE_FILE=1055@ir03lic1.ugent.be && cat $PBS_NODEFILE > fluent.hosts',
+        'abaqus.v614': 'module load intel/2018a && module load ABAQUS/6.14.1-linux-x86_64 && unset SLURM_GTIDS '
                        '&& export LM_LICENSE_FILE=@ir03lic1.ugent.be:@bump.ugent.be'
     }
 }
