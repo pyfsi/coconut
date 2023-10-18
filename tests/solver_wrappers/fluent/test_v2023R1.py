759d603f48bc4e25dd559678f0801cdb4efb088f
hdolfen <52496779+hdolfen@users.noreply.github.com>
2023-10-18 14:28:58 +0200

    Update to v2023R1

--- tests/solver_wrappers/fluent/test_v2023R1.py
+++ tests/solver_wrappers/fluent/test_v2023R1.py
@@ -4,17 +4,17 @@ from coconut.tests.solver_wrappers.fluent import fluent
 import unittest
 
 
-version = '2022R1'
+version = '2023R1'
 
 
 @unittest.skipUnless(solver_available(f'fluent.v{version}'), f'fluent.v{version} not available')
-class TestSolverWrapperFluent2022R1Tube2D(fluent.TestSolverWrapperFluentTube2D):
+class TestSolverWrapperFluent2023R1Tube2D(fluent.TestSolverWrapperFluentTube2D):
     version = version
     setup_case = True
 
 
 @unittest.skipUnless(solver_available(f'fluent.v{version}'), f'fluent.v{version} not available')
-class TestSolverWrapperFluent2022R1Tube3D(fluent.TestSolverWrapperFluentTube3D):
+class TestSolverWrapperFluent2023R1Tube3D(fluent.TestSolverWrapperFluentTube3D):
     version = version
     setup_case = True
 
