Changes: - The set_heat_flux udf now changes the sign of the heat flux values read from the file. This fixes the issue of unphysical temperatures on the interface and results in correct predictions. - Bugfix in animate_temperature.py: both the animations and temperature graph can be displayed. - Temperature at the interface is now compared to data from a Fluent validation case. This is for the two-domain conduction-only problem with 30 °C on the hot wall and 10 °C on the cold wall (domain length = 2*50 mm, k_s = 2.4 W/mK & k_l = 1.5 W/mK).

thermal_development/conduction/post_processing/animate_temperature.py
@@ -1,10 +1,13 @@
 from coconut.examples.post_processing.animate import AnimationFigure
 
+import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 import matplotlib.animation as ani
 import pickle
+
+flag = False
 
 # Initial interface temperature
 T_ini = 20 # °C
@@ -21,12 +24,12 @@ for name, path in zip(legend_entries, case_paths):
         results.update({name: pickle.load(file)})
 
 # make figure and create animation for each case
-animation_figure_temperature = AnimationFigure()  # figure for pressure animations
-animation_figure_heat_flux = AnimationFigure()  # figure for coordinate animations
+animation_figure_temperature = AnimationFigure()  # figure for temperature animations
+animation_figure_heat_flux = AnimationFigure()  # figure for heat flux animations
 colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red']
 line_styles = ['-', '--', ':', '-.']
 for sol, itf, var, uni, ani_fig in (('solution_y', 'interface_y', 'temperature', 'K', animation_figure_temperature),
-                                    ('solution_x', 'interface_x', 'heat_flux', 'W', animation_figure_heat_flux)):
+                                    ('solution_x', 'interface_x', 'heat_flux', 'W/m^2', animation_figure_heat_flux)):
     for j, name in enumerate(legend_entries):
         if var == 'temperature':
             solution = results[name][sol] - 273.15
@@ -54,17 +57,12 @@ for sol, itf, var, uni, ani_fig in (('solution_y', 'interface_y', 'temperature',
         animation.line.set_marker('o')
         animation.line.set_markersize(2)
 
-        # Plot avg interface temperature in time
+        # Store interface data for temperature plot
         if var == "temperature":
-            print(type(solution))
+            flag = True
             avg_T = np.array([np.mean(solution[:,i]) for i in range(np.shape(solution)[1])])
             avg_T[0] = T_ini
             time = time_step_start + dt*np.array(range(np.shape(solution)[1]))
-            plt.plot(time, avg_T, 'or')
-            plt.ylabel('Interface temperature [°C]')
-            plt.xlabel('Time [s]')
-            plt.ylim((np.min(avg_T)-1, np.max(avg_T)+1))
-            plt.show()
 
     ani_fig.figure.axes[0].set_ylabel(f'{var} ({uni})')
     ani_fig.figure.axes[0].set_xlabel('height on itf (m)')
@@ -86,3 +84,27 @@ if save:
     animation_figure.animation.save(movie_name, writer=writer)
 
 plt.show()
+plt.close()
+
+if flag:
+    # Plot avg interface temperature in time
+    # First, read Fluent validation files
+    read_file = pd.read_csv(r'fluent_validation.out', delimiter='\s+', skiprows=[0, 1, 2]) # Path of Fluent out-file
+    read_file.to_csv(r'fluent_validation.csv', index=None)
+    data_array = np.loadtxt('fluent_validation.csv', delimiter=',')
+    T_val = data_array[:,1] - 273.15
+    time_val = data_array[:,2]
+    try:
+        os.remove("fluent_validation.csv")
+    except:
+        pass
+
+    # Plot
+    line1, = plt.plot(time, avg_T, '*r', label="CoCoNuT")
+    line2, = plt.plot(time_val, T_val, '--g', label="Fluent")
+    plt.ylabel('Interface temperature [°C]')
+    plt.xlabel('Time [s]')
+    plt.ylim((np.min(avg_T)-1, np.max(avg_T)+1))
+    plt.legend(handles=[line1, line2])
+    plt.show()
+    plt.close()