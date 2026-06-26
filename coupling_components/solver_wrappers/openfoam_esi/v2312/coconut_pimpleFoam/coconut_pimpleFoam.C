bd798e3a5b6dbd25db4da17d2ac6654ca22eddc3
Ryan Adiputra <ryanadiputra230798@gmail.com>
2026-06-26 10:31:22 +0200

    initial commit

---
+++ coupling_components/solver_wrappers/openfoam_esi/v2312/coconut_pimpleFoam/coconut_pimpleFoam.C
@@ -0,0 +1,281 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | 
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2017 OpenFOAM Foundation
+    Copyright (C) 2019 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    coconut_pimpleFoam.C
+
+Description
+    Transient solver for incompressible, turbulent flow of Newtonian fluids 
+    on a moving mesh.
+
+    \heading Solver details
+    The solver uses the PIMPLE (merged PISO-SIMPLE) algorithm to solve the 
+    continuity equation:
+
+        \f[
+            \div \vec{U} = 0
+        \f]
+
+    and momentum equation:
+        \f[
+            \ddt{\vec{U}} + \div \left( \vec{U} \vec{U} \right) - \div \gvec{R}
+            = - \grad p + \vec{S}_U
+        \f]
+
+    Where:
+    \vartable
+        \vec{U} | Velocity
+        p       | Pressure
+        \vec{R} | Stress tensor
+        \vec{S}_U   | Momentum source
+    \endvartable
+
+    Sub-models include:
+    - turbulence modelling, i.e. laminar, RAS or LES
+    - run-time selectable MRF and finite volume options, e.g. explicit porosity
+
+    \heading Required fields
+    \plaintable
+        U       | Velocity [m/s]
+        p       | Kinematic pressure, p/rho [m2/s2]
+        \<turbulence fields\> | As required by user selection
+    \endplaintable
+
+Note
+   The motion frequency of this solver can be influenced by the presence
+   of "updateControl" and "updateInterval" in the dynamicMeshDict.
+
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "dynamicFvMesh.H"
+#include "singlePhaseTransportModel.H"
+#include "turbulentTransportModel.H"
+
+#include "pimpleControl.H"
+// #include "pressureReference.H"
+#include "CorrectPhi.H"
+// #include "fvModels.H"
+#include "fvOptions.H"
+// #include "fvConstraints.H"
+#include "localEulerDdtScheme.H"
+#include "fvcSmooth.H"
+
+// Coconut-specific headers
+#include "fsiDisplacement.H"
+#include "waitForSync.H"
+#include <unistd.h>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+
+
+int main(int argc, char *argv[])
+{
+    argList::addNote
+    (
+        "Transient solver for incompressible, turbulent flow"
+        " of Newtonian fluids on a moving mesh."
+    );
+
+    #include "postProcess.H"
+
+    #include "addCheckCaseOptions.H"
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    // #include "createMesh.H"
+    #include "createDynamicFvMesh.H"
+    #include "initContinuityErrs.H"
+    #include "createDyMControls.H"
+    #include "createFields.H"
+    #include "createUfIfPresent.H"
+    // why this necessary (?). choose this or the one inside !LTS
+    #include "CourantNo.H"
+    #include "setInitialDeltaT.H"
+
+    turbulence->validate();
+
+    if (!LTS)
+    {
+        #include "CourantNo.H"
+        #include "setInitialDeltaT.H"
+    }
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    unsigned int iteration;
+    iteration = 0;
+
+    #include "readCoconutControls.H"
+
+    Info << "Starting loop" << nl << endl;
+
+    while (true)
+    {
+        usleep(1000); // Expressed in microseconds
+
+        if (exists("next.coco"))
+        {
+            waitForSync("next"); // Keep the sync at the beginning of the block
+
+            Info << "runtime.run()" << nl << endl;
+            runTime.run();
+
+            if (LTS)
+            {
+                #include "readDyMControls.H"
+            } else {
+                #include "CourantNo.H"
+                #include "setDeltaT.H"
+            }            
+
+            // TODO
+            // Info << "fvModels.preUpdateMesh()" << nl << endl;
+            // fvModels.preUpdateMesh();
+
+            // // Update the mesh for topology change, mesh to mesh mapping
+            // mesh.update();
+
+            // TODO HERE. ERROR ??
+            // mesh.controlledUpdate();
+
+            runTime++;
+            iteration = 0;
+
+            Info << "Time = " << runTime.timeName() << nl << endl;
+            Info << "FINISHED NEXT.COCO" << nl << endl;
+        }
+
+        if (exists("continue.coco"))
+        {
+            waitForSync("continue");
+
+            iteration++;
+            Info << "Coupling iteration = " << iteration << nl << endl;
+
+            // Define movement of the coupling interface
+            forAll(boundaryNames, s)
+            {
+                word boundaryName = boundaryNames[s];
+                ApplyFSIPointDisplacement(mesh, boundaryName);
+            }
+
+            // --- Pressure-velocity PIMPLE corrector loop
+            while (pimple.loop())
+            {
+                if (pimple.firstIter() || moveMeshOuterCorrectors)
+                {
+                    // TODO Calculate the mesh motion and update the mesh
+                    // mesh.move();
+
+                    // Do any mesh changes
+                    mesh.controlledUpdate();
+
+                    if (mesh.changing())
+                    {
+                        Info << "Inside changing mesh" << nl << endl;
+ 
+                        MRF.update();
+
+                        if (correctPhi)
+                        {
+                            // Calculate absolute flux
+                            // from the mapped surface velocity
+                            phi =mesh.Sf() & Uf();
+
+                            #include "correctPhi.H"
+
+                            // Make the flux relative to the mesh motion
+                            fvc::makeRelative(phi, U);
+                        }
+
+                        if (checkMeshCourantNo)
+                        {
+                            #include "meshCourantNo.H"
+                        }
+                    }
+                }
+
+                // TODO fvModels.correct();
+
+                #include "UEqn.H"
+
+                // --- Pressure corrector loop
+                while (pimple.correct())
+                {
+                    #include "pEqn.H"
+                }
+
+                if (pimple.turbCorr())
+                {
+                    // TODO viscosity->correct();
+                    laminarTransport.correct();
+                    turbulence->correct();
+                }
+
+                // Check coupling convergence
+                if (checkCouplingConvergence && pimple.firstIter())
+                {
+                    #include "checkCouplingConvergence.H"
+                }
+            }
+
+            Info << "END PIMPLE LOOP" << nl << endl;
+
+            Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+                << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+                << nl << endl;
+
+            // Return the coupling interface output
+            #include "executeCoconutFunctionObjects.H"
+
+            Info << "Coupling iteration " << iteration << " end" << nl << endl;
+            Info << "FINISHED CONTINUE.COCO" << nl << endl;
+        }
+
+        if (exists("save.coco"))
+        {
+            waitForSync("save");
+
+            runTime.write(); // OF-command: loops over all objects and requests writing
+            // writing is done based on the specific settings of each variable (AUTO_WRITE, NO_WRITE)
+        }
+
+        if (exists("stop.coco"))
+        {
+            waitForSync("stop");
+            break;
+        }
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
